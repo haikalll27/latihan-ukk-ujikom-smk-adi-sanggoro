0353eda2286b22c1f17b3e64495fb3e5d2432f39
Cleanidbogor <cleanidbogo@gmail.com>
2024-11-29 01:14:44 +0700

    push new ke dua

--- latihan-ukk-2.php
+++ latihan-ukk-2.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <?php include './layout/head-latihan-ukk.php'; ?>
 </head>
 <body>
+
+ <?php include './layout/header-latihan-ukk.php'; ?>
 
 <?php 
 
@@ -20,45 +20,52 @@ if(isset($_POST['woke'])) {
     $kelas = $_POST['kelas'];
     ?>
 
-<div>
-    <p>Guru : <?= $guru ?></p>
-    <p>Mapel : <?= $mapel ?></p>
-    <p>KKM : <?= $kkm ?></p>
-    <p>Kelas : <?= $kelas ?></p>
+<div class="grup-all-data-1">
+    <div class="grup-all-data-2">
+        <div class="grup-peserta-didik">
+            <img class="img-as-two" src="./assets/img/LOGO.png" alt="">
+            <h2 class="heading-2-rekap-perserta-didik">Hasil Data Nilai Siswa</h2>
+         </div>
+        <p class="text-all-data">Guru : <?= $guru ?></p>
+        <p class="text-all-data">Mapel : <?= $mapel ?></p>
+        <p class="text-all-data">KKM : <?= $kkm ?></p>
+        <p class="text-all-data">Kelas : <?= $kelas ?></p>
+
+        <form action="" method="">
+            <table for="" border="1">
+                <tr>
+                    <th>No</th>
+                    <th>NIS</th>
+                    <th>Nama</th>
+                    <th>Nilai</th>
+                    <th>Keterangan</th>
+                </tr>
+                <?php 
+                for ($i = 0; $i < count(value: $nis); $i++) {
+                    ?>
+                    <tr>
+                        <td><?= $i+1; ?></td>
+                        <td><?= $nis[$i]; ?></td>
+                        <td><?= $nama[$i]; ?></td>
+                        <td><?= $nilai[$i]; ?></td>
+                        <td>
+                            <?php 
+                            if ($nilai[$i] > $kkm) {
+                                echo 'Tuntas';
+                            } else {
+                                echo 'Remedial Kasihan Deloh';
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                    <?php
+                }
+                ?>
+            </table>
+        </form>
+    </div>
 </div>
 
-    <form action="" method="">
-        <table for="" border="1">
-            <tr>
-                <th>No</th>
-                <th>NIS</th>
-                <th>Nama</th>
-                <th>Nilai</th>
-                <th>Keterangan</th>
-            </tr>
-            <?php 
-            for ($i = 0; $i < count(value: $nis); $i++) {
-                ?>
-                <tr>
-                    <td><?= $i+1; ?></td>
-                    <td><?= $nis[$i]; ?></td>
-                    <td><?= $nama[$i]; ?></td>
-                    <td><?= $nilai[$i]; ?></td>
-                    <td>
-                        <?php 
-                        if ($nilai[$i] > $kkm) {
-                            echo 'Tuntas';
-                        } else {
-                            echo 'Remedial Kasihan Deloh';
-                        }
-                        ?>
-                    </td>
-                </tr>
-                <?php
-            }
-            ?>
-        </table>
-    </form>
 <?php
     }
 ?>    
--- layout/head-latihan-ukk.php
+++ layout/head-latihan-ukk.php
@@ -1,6 +1,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+<link rel="icon" href="./assets/img/smk-as.png" type="image/png">
+
 <link rel="stylesheet" href="./assets/css/head-latihan-ukk.css">
 
 <title>Ukk 1</title>
